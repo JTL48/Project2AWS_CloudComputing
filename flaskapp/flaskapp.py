9402f3dde03fcc2d0639e7a790335ff483f8413f
Ubuntu <ubuntu@ip-172-31-40-28.us-east-2.compute.internal>
2023-02-05 23:06:20 +0000

    final commit

--- flaskapp/flaskapp.py
+++ flaskapp/flaskapp.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-from flask import Flask, request, g, render_template, url_for, flash, redirect
+from flask import Flask, request, g, render_template, url_for, flash, redirect, json
 from collections import Counter
 app = Flask(__name__)
 
@@ -43,7 +43,9 @@ def execute_query(query, args=()):
 @app.route("/viewdb")
 def viewdb():
     rows = execute_query("""SELECT * FROM AWSusers""")
-    return '<br>'.join(str(row) for row in rows)
+    htmlStr = '<br>'.join(str(row) for row in rows)
+    htmlStr = htmlStr + '<br><br><a href=\"/\"><button>Back Home</button></a>'
+    return htmlStr
 
 @app.route("/username/<username>")
 def sortby(username):
@@ -56,19 +58,106 @@ def createLogin():
         username = request.form['username']
         password = request.form['password']
 
+        message = json.dumps({"username":username,"password":password})
+
         if not username:
             flash('Username is required!')
         elif not password:
             flash('Password is required!')
         else:
             conn = get_db()
-            conn.execute("INSERT INTO AWSusers (Username, Password) VALUES (?, ?)",
-                (username, password))
-            conn.commit()
-            conn.close()
-            return redirect(url_for('index'))
+            rows = conn.execute("SELECT FirstName, LastName, Email FROM AWSusers WHERE Username = ? AND Password = ?",
+                (username,password)).fetchone()
+
+            if rows is None:
+                conn.execute("INSERT INTO AWSusers (Username, Password) VALUES (?, ?)",
+                    (username, password))
+                conn.commit()
+                conn.close()
+                return redirect(url_for('createProfile',message = message))
+            else:
+                htmlStr = 'Username and Password Pair already exists'
+                htmlStr = htmlStr + '<br><br><a href=\"/\"><button>Back Home</button></a>'
+                return htmlStr
 
     return render_template('createLogin.html')
+
+@app.route("/createProfile/", methods=('GET', 'POST'))
+def createProfile():
+    messageTemp = request.args.get("message")
+    message = json.loads(messageTemp)
+    if request.method == 'POST':
+        firstName = request.form['firstName']
+        lastName = request.form['lastName']
+        email = request.form['email']
+        username = message['username']
+        password = message['password']
+        newMessage = json.dumps({"username":username,"password":password})
+
+        if not firstName:
+            flash('First Name is required!')
+        elif not lastName:
+            flash('Last Name is required!')
+        elif not email:
+            flash('Email is required!')
+        else:
+            conn = get_db()
+            conn.execute("UPDATE AWSusers SET FirstName = ?, LastName = ?, Email = ? WHERE Username = ? AND Password = ?",
+                (firstName, lastName, email, username, password))
+            conn.commit()
+            conn.close()
+
+            return redirect(url_for('showProfile',message = newMessage))
+
+    return render_template('createProfile.html')
+
+@app.route("/showProfile/")
+def showProfile():
+    messageTemp = request.args.get("message")
+    message = json.loads(messageTemp)
+    username = message['username']
+    password = message['password']
+    conn = get_db()
+    rows = conn.execute("SELECT FirstName, LastName, Email FROM AWSusers WHERE Username = ? AND Password = ?",
+        (username,password)).fetchone()
+    conn.close()
+    htmlStr = '<br>'.join(str(row) for row in rows)
+    htmlStr = htmlStr + '<br><br><a href=\"/\"><button>Back Home</button></a>'
+    return htmlStr
+
+@app.route("/findUser/", methods=('GET', 'POST'))
+def findUser():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        message = json.dumps({"username":username,"password":password})
+
+        if not username:
+            flash('Username is required!')
+        elif not password:
+            flash('Password is required!')
+        else:
+            return redirect(url_for('findUserResult',message = message))
+
+    return render_template('findUser.html')
+
+@app.route("/findUserResult/")
+def findUserResult():
+    messageTemp = request.args.get("message")
+    message = json.loads(messageTemp)
+    username = message['username']
+    password = message['password']
+    conn = get_db()
+    rows = conn.execute("SELECT FirstName, LastName, Email FROM AWSusers WHERE Username = ? AND Password = ?",
+        (username,password)).fetchone()
+    conn.close()
+    if rows is None:
+        htmlStr = 'Invalid Username or Password'
+    else:
+        htmlStr = '<br>'.join(str(row) for row in rows)
+    htmlStr = htmlStr + '<br><br><a href=\"/\"><button>Back Home</button></a>'
+    return htmlStr
 
 if __name__ == '__main__':
   app.run(debug=True)
